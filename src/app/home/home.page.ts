859494339fc07140ee3fcb233765adfb7d137c76
Edra13 <edraelricadewi@mail.ugm.ac.id>
2024-09-23 10:54:16 +0700

    PGPBL Acara 6

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,39 +16,51 @@ export class HomePage {
   ngOnInit() {}
 
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView([51.505, -0.09], 10);
+    this.map = L.map('mapId').setView([-7.816795, 110.365229], 13);
 
     // Basemap pertama (OpenStreetMap)
-    const osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const basemap1 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
     // Basemap kedua (CartoDB Positron)
-    const positronLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    const basemap2 = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd'
     });
 
-    // Basemap ketiga (Esri World Imagery)
-    const esriImageryLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{x}/{y}', {
-      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    // Basemap keempat (Stadia Alidade Smooth Dark)
+    const basemap3 = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    });
+
+    // Basemap kelima (Stadia Alidade Smooth)
+    const basemap4 = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // Basemap keenam (Stadia Stamen Terrain Background)
+    const basemap5 = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain_background/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://www.stamen.com/">Stamen Design</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
     // Tambahkan layer kontrol
     const baseMaps = {
-      "OpenStreetMap": osmLayer,
-      "CartoDB Positron": positronLayer,
-      "Esri World Imagery": esriImageryLayer
+      "OpenStreetMap": basemap1,
+      "CartoDB Positron": basemap2,
+      "Alidade Smooth Dark": basemap3,
+      "Alidade Smooth": basemap4,
+      "Stamen Terrain Background": basemap5
     };
 
     // Tambahkan layer pertama sebagai default
-    osmLayer.addTo(this.map);
+    basemap1.addTo(this.map);
 
     // Kontrol layer
     L.control.layers(baseMaps).addTo(this.map);
 
     // Tambahkan marker
-    this.marker = L.marker([51.505, -0.09]).addTo(this.map);
-    this.marker.bindPopup('Informasi Pop-up: Saya ada di sini!').openPopup();
+    this.marker = L.marker([-7.816795, 110.365229]).addTo(this.map);
+    this.marker.bindPopup('Hi, this is my home!').openPopup();
   }
 }
